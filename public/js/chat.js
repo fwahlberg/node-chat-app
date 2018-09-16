cc624651bdcff8a226c8508cb4e6e2da8ef82298
Felix Wahlberg <felixwahlberg@felixs-mbp.home>
2018-09-16 20:08:41 +0100

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+
+  var scrollTop = messages.prop('scrollTop');
+
+  var scrollHeight = messages.prop('scrollHeight');
+
+  var newMessageHeight = newMessage.innerHeight();
+
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +37,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.data}`);
@@ -35,6 +55,8 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+
+  scrollToBottom();
 
 });
 
