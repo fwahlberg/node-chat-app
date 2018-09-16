9a8161813a3302d563f4f60aa17cc74ede85e9eb
Felix Wahlberg <felixwahlberg@felixs-mbp.home>
2018-09-16 19:16:30 +0100

    Included timestamps

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,16 +1,17 @@
+var moment = require('moment');
 class Message {
   constructor(from = '', data = null, location = false) {
     if (!location) {
       return {
         from,
         data,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
       }
     } else {
       return {
         from,
         url: `https://maps.google.com?q=${data.latitude},${data.longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
       }
     }
   }
